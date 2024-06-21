c99590aa695173cc74c27707aa9a90e6ed9c1783
shivammodiin <shivammodi456@gmail.com>
2024-06-21 11:19:27 +0530

    code and test cases for one and more arguments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export function calculateStringSum(input: string): number {
+    let delimiter = ",";                                    
 
     if(input === '') return 0;
+    const numberStrings: string[] = input.split(delimiter); 
+    const numbersArray: number[] = numberStrings.map(str => +str);
 
-    return 0;
+    return numbersArray.reduce((sum, num) => sum + num, 0);
+
 
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,4 +5,12 @@ describe("String Calculator", () => {
     it("returns 0 for an empty string", () => {
         expect(calculateStringSum("")).toEqual(0);
     });
+
+    it('returns 2 when the input is "2"', () => {
+        expect(calculateStringSum("2")).toEqual(2);
+    });
+      
+    it('returns 13 for the input "7,6"', () => {
+    expect(calculateStringSum("7,6")).toEqual(13);
+    });
 });
